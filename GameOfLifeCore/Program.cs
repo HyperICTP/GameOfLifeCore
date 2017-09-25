f49074c811e7ac8a1c9a55cfab8ee3eea0dcca83
SoftwareEngineeringPractice2017 <31974523+SoftwareEngineeringPractice2017@users.noreply.github.com>
2017-09-26 06:51:51 +1000

    Add project files.

---
+++ GameOfLifeCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GameOfLifeCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello HyperScalers, This is running on OpenShift!");
+            Console.WriteLine( "^_^" );
+        }
+    }
+}
